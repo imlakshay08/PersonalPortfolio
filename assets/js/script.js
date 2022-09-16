de4c82d7ec6fde71b5fdd5bdbbf927d78ec07d21
Lakshay <lakshay@debian-BULLSEYE-live-builder-AMD64>
2022-09-16 12:26:01 +0530

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["full stack development", "open-source", "penetration testing", "we development" ],
+    strings: ["full stack development", "open-source", "penetration testing", "web development" ],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
