a30357a855ab520568b7d8343d7bfd04cd8d66e4
Lakshay <lakshay@debian-BULLSEYE-live-builder-AMD64>
2022-09-16 06:11:40 +0530

    add portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "open-source", "full stack development"],
+    strings: ["full stack development", "open-source", "penetration testing", "we development" ],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
