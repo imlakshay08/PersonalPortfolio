40212d12421d1b59d97cbc4f9c003af6bde37bf7
Lakshay Tyagi <tyagilakshay119@gmail.com>
2025-11-02 22:57:47 +0530

    Changed profile image, description content

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["full stack development", "open-source", "penetration testing", "web development" ],
+    strings: ["Software Development", "Ruby on Rails", "Java", "Open Source" ],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
